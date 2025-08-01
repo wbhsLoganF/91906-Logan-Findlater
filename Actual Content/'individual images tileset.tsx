b07387c6c6a01572769bc93f1ab2f6a4e282bc26
Logan Findlater <lf21549@my.westlake.school.nz>
2025-08-01 15:24:47 +1200

    Final commit 1/08

--- Actual Content/'individual images tileset.tsx
+++ Actual Content/'individual images tileset.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="'individual images tileset" tilewidth="164" tileheight="128" tilecount="27" columns="0">
+<tileset version="1.10" tiledversion="1.11.2" name="'individual images tileset" tilewidth="164" tileheight="128" tilecount="28" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image source="roof_cave_trans.png" width="164" height="64"/>
@@ -102,5 +102,8 @@
  </tile>
  <tile id="37">
   <image source="transparent_block.png" width="64" height="64"/>
+ </tile>
+ <tile id="41">
+  <image source="long_door.png" width="85" height="128"/>
  </tile>
 </tileset>
